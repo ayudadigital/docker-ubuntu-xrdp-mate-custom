78f78c4ee3cdf3fabf2db5a2433f1a594e89e847
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-04-14 13:05:17 +0200

    Breaking: Change kairops references with tpbtools

--- duing/run.sh
+++ duing/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker run -d --name duing \
+docker run --name duing \
            -p 3389:3389 \
            --shm-size 1g \
            -dit --restart unless-stopped \
-           kairops/duing
+           tpbtools/duing
